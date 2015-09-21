bbde483ad2bd6e022a522dc22c6094d19d151172
jayeshjoy <jayjoy88@gmail.com>
2015-09-20 21:55:11 -0700

    Initial Commit

---
+++ tipsy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tipsy
+//
+//  Created by Jayesh Joy on 9/20/15.
+//  Copyright © 2015 Jayesh Joy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tipsy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipsy
+//
+//  Created by Jayesh Joy on 9/20/15.
+//  Copyright © 2015 Jayesh Joy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
